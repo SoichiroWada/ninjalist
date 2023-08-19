7d5d989b61c8b476e27b2fc643a0b87bfc5370ea
SoichiroWada <soichirowada@ipmulticast.net>
2023-08-19 18:45:57 +0900

    second

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,10 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1>Hello!</h1>
+    <div>
+      <h1>Homepage</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
     </div>
   )
 }
---
+++ pages/ninjas/index.js
@@ -0,0 +1,9 @@
+const View = () => {
+    return (
+        <div>
+            <h3>View</h3>
+        </div>
+    );
+}
+ 
+export default View;
