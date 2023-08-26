c5e7729ad2c6b6e3f7cfef09523d50ef7f39df74
SoichiroWada <soichirowada@ipmulticast.net>
2023-08-27 00:01:30 +0900

    json data fetch

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
+import Image from 'next/image'
 
 const Navbar = () => {
     return (
         <nav>
             <div className="logo">
-                <h1>Ninja List</h1>
+                <Link href="/"><Image src="/logo.png" width={128} height={77} alt="ninjalogo" /></Link>
             </div>
             <Link href="/">Home</Link>
             <Link href="/about">About</Link>
--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,9 +1,28 @@
-const View = () => {
+import sytles from '../../styles/Ninjas.module.css'
+
+export const getStaticProps = async () => {
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await res.json();
+
+    return {
+        props: { ninjas: data }
+    }
+}
+
+const Ninjas = ({ ninjas }) => {
     return (
         <div>
             <h1>All Ninjas</h1>
+            {ninjas.map(ninjas => (
+                <div key={ninjas.id}>
+                    <a className={sytles.single}>
+                        <h3>{ninjas.name}</h3>
+                    </a>
+                </div>
+            ))}
         </div>
     );
 }
  
-export default View;
+export default Ninjas;
