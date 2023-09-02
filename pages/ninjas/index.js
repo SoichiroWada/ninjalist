3291ed94edbc3aefae05949aa07e68103a837958
SoichiroWada <soichirowada@ipmulticast.net>
2023-09-02 20:04:25 +0900

    hello 6

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,12 +1,10 @@
-import Link from 'next/link';
-import sytles from '../../styles/Ninjas.module.css'
+import styles from '../../styles/Ninjas.module.css'
+import Link from 'next/link'
 
 export const getStaticProps = async () => {
 
-    const res = await fetch('https://jsonplaceholder.typicode.com/users')
-    // console.log(res)
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
     const data = await res.json();
-    // console.log("data:", data)
 
     return {
         props: { ninjas: data }
@@ -17,10 +15,10 @@ const Ninjas = ({ ninjas }) => {
     return (
         <div>
             <h1>All Ninjas</h1>
-            {ninjas.map(ninjas => (
-                <Link href={'/ninjas/' + ninjas.id} key={ninjas.id}>
-                    <div className={sytles.single}>
-                        <h3>{ninjas.name}</h3>
+            {ninjas.map(ninja => (
+                <Link href={'/ninjas/' + ninja.id} key={ninja.id}>
+                    <div className={styles.single}>
+                        <h3>{ ninja.name }</h3>
                     </div>
                 </Link>
             ))}
